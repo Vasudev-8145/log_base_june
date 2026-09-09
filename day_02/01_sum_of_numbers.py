9e01a3ab0cd86b546bc57d0affb4dfa5e6729b2d
vasudev <vasudevc8145@gmail.com>
2026-09-09 18:40:06 +0530

    day 02 task 1

--- day_02/01_sum_of_numbers.py
+++ day_02/01_sum_of_numbers.py
@@ -1,3 +1,14 @@
 """
 write a program to display sum of number from  1 to n
 """
+
+n = int(input("Enter upper limit:"))
+
+sum = 0
+
+for i in range(1,n+1):
+
+    sum+=i
+    i+=1
+
+print(f"sum = {sum}")
