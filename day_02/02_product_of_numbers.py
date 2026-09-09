be4dd1132c73240ff6f32da3f8e7fa44a812578f
vasudev <vasudevc8145@gmail.com>
2026-09-09 18:41:32 +0530

    day 02 task 2

--- day_02/02_product_of_numbers.py
+++ day_02/02_product_of_numbers.py
@@ -1,3 +1,14 @@
 """
 write a program to display product of number from  1 to n
 """
+
+n = int(input("Enter upper limit:"))
+
+product = 1
+
+for i in range(1,n+1):
+
+    product*=i
+    i+=1
+
+print(f"product = {product}")
