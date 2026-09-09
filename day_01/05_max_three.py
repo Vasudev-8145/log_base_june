01ad0f3800fca205f0bf5fcde1995c512f843bad
vasudev <vasudevc8145@gmail.com>
2026-09-09 18:34:17 +0530

    day 01 task 5

--- day_01/05_max_three.py
+++ day_01/05_max_three.py
@@ -4,3 +4,16 @@
 write a program to print largest of three numbers
 
 """
+
+num1 = int(input("Enter a num1:"))
+num2 = int(input("Enter a num2:"))
+num3 = int(input("Enter a num3:"))
+
+if num1>num2 and num1>num3:
+    print(f"{num1} is greatest")
+
+elif num2>num1 and num2>num3:
+    print(f"{num2} is greatest")
+
+elif num3>num1 and num3>num1:
+    print(f"{num3} is greatest")
