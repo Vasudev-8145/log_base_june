e3e06b3c60f61682f937c42a1f8e694584d55c60
vasudev <vasudevc8145@gmail.com>
2026-09-09 18:37:55 +0530

    day 01 task 4

--- day_01/04_leap_year.py
+++ day_01/04_leap_year.py
@@ -2,3 +2,11 @@
 write a program to chk year is leap year or not
 
 """
+
+year = int(input("Enter a number:"))
+
+if (year%100==0 and year%400==0) or (year%100!=0 and year%4==0):
+    print("Leap year")
+
+else:
+    print("Not a leap year")
