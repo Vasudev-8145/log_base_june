e15a366f9888a1be432a7f2c7ef1dec68b358ab1
vasudev <vasudevc8145@gmail.com>
2026-09-09 18:30:40 +0530

    day o1 task 1

--- day_01/01_number_check.py
+++ day_01/01_number_check.py
@@ -2,3 +2,14 @@
 write a program to check number is +ve -ve or zero
 
 """
+
+num = int(input("Enter a number:"))
+
+if num == 0:
+    print("zero")
+
+elif num>0:
+    print("positive")
+
+else:
+    print("Negative")
