4b9e52f7a51edc2ed86c2f190361121d6d0f81ef
vasudev <vasudevc8145@gmail.com>
2026-09-09 18:43:44 +0530

    day 02 task 3

--- day_02/03_prime_number.py
+++ day_02/03_prime_number.py
@@ -2,3 +2,16 @@
 write a program chk number is prime or not
 
 """
+
+num = int(input("Enter a number:"))
+
+for i in range(2,num):
+
+    if num%i == 0:
+        print("Number is not prime")
+        break
+
+else:
+    print("prime number")
+
+
