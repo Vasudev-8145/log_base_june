e87d509b79e84568fe054d09a1d6ca2838cc1968
vasudev <vasudevc8145@gmail.com>
2026-09-09 18:32:43 +0530

    day 01 task 2

--- day_01/02_largest_of_two_numbers.py
+++ day_01/02_largest_of_two_numbers.py
@@ -2,3 +2,12 @@
 write a program to chk largest of two numbers
 
 """
+
+num1 = int(input("Enter a num1:"))
+num2 = int(input("Enter a num2:"))
+
+if num1>num2:
+    print(f"{num1} is greatest")
+
+elif num2>num1:
+    print(f"{num2} is greatest")
